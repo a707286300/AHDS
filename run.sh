c4884a77c192238a7a24749b4ee3d790db9af665
Yichen <280412681@qq.com>
2024-12-01 10:30:50 +0000

    slurm

--- run.sh
+++ run.sh
@@ -4,22 +4,21 @@
 set -e
 
 # SLURM Job Submission Directives
-#SBATCH --job-name=AHDS          # Name of the job
-#SBATCH --partition=teach_cpu            # Partition name
-#SBATCH --nodes=1                        # Number of nodes
-#SBATCH --ntasks=1                       # Number of tasks (typically the same as nodes for simple jobs)
-#SBATCH --cpus-per-task=1                # Number of cores per task
-#SBATCH --mem=16G                        # Memory per node
-#SBATCH --time=12:00:00                  # Time limit hrs:min:sec
+#SBATCH --job-name=AHDS        
+#SBATCH --partition=teach_cpu           
+#SBATCH --nodes=1                       
+#SBATCH --ntasks=1                     
+#SBATCH --cpus-per-task=1                
+#SBATCH --mem=16G                        
+#SBATCH --time=12:00:00                  
 #SBATCH --output=AHDS%j.log     
 #SBATCH --account=SSCM033324
 
-echo "Job started on $(date)"  # Using $(...) for command substitution
+echo "Job started on $(date)"  
 
 # Conda environment
 source ~/initConda.sh
-conda activate AHDS-env
+conda activate ahds_env
 
 snakemake
 
-echo "Job ended on $(date)"  
