e139b3615731522e6b825b179a685cbe6b3050d9
Yichen <280412681@qq.com>
2024-12-01 09:50:23 +0000

    result

--- scripts/step1.sh
+++ scripts/step1.sh
@@ -3,7 +3,7 @@
 mkdir -p raw/data
 # Fetch PubMed IDs using  query
 echo "Retrieving PubMed IDs..."
-curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=%22long%20covid%22&retmax=10000" > raw/data/pmids.xml
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=%22long%20covid%22&retmax=10" > raw/data/pmids.xml
 echo "Extracting PubMed IDs from XML..."
 pmids=$(grep -oP '(?<=<Id>)[0-9]+(?=</Id>)' raw/data/pmids.xml)
 # Check if the output file was created
